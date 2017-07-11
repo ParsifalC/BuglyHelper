e77cf5b054e8d72b10e6a3b1aa53f6e12ed5178d
Parsifal <zmw@izmw.me>
2017-07-11 10:32:21 +0800

    add NSDate+Util.

---
+++ BuglyHelper/Category/NSDate+Util.h
@@ -0,0 +1,15 @@
+//
+//  NSDate+Util.h
+//  BuglyHelper
+//
+//  Created by Parsifal on 2017/7/11.
+//  Copyright © 2017年 Parsifal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (Util)
+
++ (NSDate *)yesterday;
+
+@end
---
+++ BuglyHelper/Category/NSDate+Util.m
@@ -0,0 +1,18 @@
+//
+//  NSDate+Util.m
+//  BuglyHelper
+//
+//  Created by Parsifal on 2017/7/11.
+//  Copyright © 2017年 Parsifal. All rights reserved.
+//
+
+#import "NSDate+Util.h"
+
+@implementation NSDate (Util)
+
++ (NSDate *)yesterday {
+    NSDate *today = [NSDate date];
+    return [today dateByAddingTimeInterval:-86400];
+}
+
+@end
